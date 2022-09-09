94fb70903b20004bc1522d3c9c0747094c1e343d
Obed Olubwa <obedkimm@gmail.com>
2022-09-09 07:20:56 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 
+
 /**
  *
  * * main - print the string in the put function
@@ -21,5 +22,6 @@ int main(void)
 
 		puts("\"Programming is like building a multilingual puzzle");
 
-			return (0) 
+			return (0);
+
 }
